fabb80311bf156d9d19807b778b39a97aaa24325
Matthew Landauer <mlandauer@gmail.com>
2019-06-10 01:50:37 +1000

    Update to latest version of the gem

--- scraper.rb
+++ scraper.rb
@@ -1,3 +1,3 @@
 require "icon_scraper"
 
-IconScraper.rest_xml("http://www.eservices.lmc.nsw.gov.au/ApplicationTracking/Pages/XC.Track/SearchApplication.aspx", "d=last14days&k=LodgementDate&t=161&o=xml")
+IconScraper.scrape_and_save(:leichhardt)
